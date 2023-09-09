9fe436d5bbb06851930c34c54f48ef627cfb17a0
clarefausty <clarefausty@gmail.com>
2023-09-09 21:47:23 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const navBarLink = document.getElementsByClassName("navbar-links")[0]
 const menuIcon = document.getElementsByClassName("menu-icon")[0]
+const element = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const now = new Date();
+const currentDay = now.getDay()
+
 
 menuIcon.addEventListener('click', function(){
     navBarLink.classList.toggle('active')
 })
+element.innerHTML = days[currentDay]
+currentTime.innerHTML = now.getTime()
+
+
