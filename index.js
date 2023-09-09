cd69cd2ea691406de34f2a36500136d4d0eac855
clarefausty <clarefausty@gmail.com>
2023-09-09 22:14:11 +0100

    Modified index.css

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const navBarLink = document.getElementsByClassName("navbar-links")[0]
 const menuIcon = document.getElementsByClassName("menu-icon")[0]
 const element = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const now = new Date();
 const currentDay = now.getDay()
 
@@ -13,5 +15,6 @@ menuIcon.addEventListener('click', function(){
 })
 element.innerHTML = days[currentDay]
 currentTime.innerHTML = now.getTime()
+
 
 
